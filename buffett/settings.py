829f38eac06c37f04f34ea221435372eb7d7f790
Bruno L. Carli <brunolcarli@gmail.com>
2023-03-03 23:34:27 -0300

    CHANGE version

--- buffett/settings.py
+++ buffett/settings.py
@@ -16,7 +16,7 @@ import uuid
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
