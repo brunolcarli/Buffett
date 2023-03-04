b2bffa000594d052a74e44c32e0ce9d4aac548fe
Bruno L. Carli <brunolcarli@gmail.com>
2023-03-03 23:33:40 -0300

    CHANGE allow null fields

--- api/models.py
+++ api/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class PriceBars(models.Model):
     datetime_reference = models.DateTimeField()
-    open = models.FloatField()
-    close = models.FloatField()
-    high = models.FloatField()
-    low = models.FloatField()
+    open = models.FloatField(null=True)
+    close = models.FloatField(null=True)
+    high = models.FloatField(null=True)
+    low = models.FloatField(null=True)
 
 
 class PartialPriceBar(models.Model):
     datetime_reference = models.DateTimeField()
-    open = models.FloatField()
-    close = models.FloatField()
-    high = models.FloatField()
-    low = models.FloatField()
+    open = models.FloatField(null=True)
+    close = models.FloatField(null=True)
+    high = models.FloatField(null=True)
+    low = models.FloatField(null=True)
