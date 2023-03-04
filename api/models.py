22aff4eaacd0bdb5581d96681af65126f807c6b2
Bruno L. Carli <brunolcarli@gmail.com>
2023-03-03 21:18:10 -0300

    ADD database models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class PriceBars(models.Model):
+    datetime_reference = models.DateTimeField()
+    open = models.FloatField()
+    close = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+
+
+class PartialPriceBar(models.Model):
+    datetime_reference = models.DateTimeField()
+    open = models.FloatField()
+    close = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
