c89d7b36dffc2c541190aaedea47cf4d43aee72c
Bruno L. Carli <brunolcarli@gmail.com>
2023-03-03 21:33:15 -0300

    ADD price bars queries

--- api/schema.py
+++ api/schema.py
@@ -1,8 +1,37 @@
 import graphene
 from django.conf import settings
+from api.models import PriceBars, PartialPriceBar
+
+
+
+class PriceBarsType(graphene.ObjectType):
+    datetime_reference = graphene.DateTime()
+    open = graphene.Float()
+    close = graphene.Float()
+    high = graphene.Float()
+    low = graphene.Float()
+
+
+class PartialPriceBarType(graphene.ObjectType):
+    datetime_reference = graphene.DateTime()
+    open = graphene.Float()
+    close = graphene.Float()
+    high = graphene.Float()
+    low = graphene.Float()
+
 
 
 class Query:
     version = graphene.String()
     def resolve_version(self, info, **kwargs):
         return settings.VERSION
+
+
+    price_bars = graphene.List(PriceBarsType)
+    def resolve_price_bars(self, info, **kwargs):
+        return PriceBars.objects.filter(**kwargs)
+
+    partial_price_bar = graphene.List(PriceBarsType)
+    def resolve_partial_price_bar(self, info, **kwargs):
+        return PartialPriceBar.objects.filter(**kwargs)
+
