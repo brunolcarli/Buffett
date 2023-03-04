7c831934c0bbaae12f66efdf082f8f26e57608e7
Bruno L. Carli <brunolcarli@gmail.com>
2023-03-03 21:38:33 -0300

    ADD query filters

--- api/schema.py
+++ api/schema.py
@@ -27,11 +27,46 @@ class Query:
         return settings.VERSION
 
 
-    price_bars = graphene.List(PriceBarsType)
+    price_bars = graphene.List(
+        PriceBarsType,
+        # date filters
+        datetime_reference__gte=graphene.Date(),
+        datetime_reference__lte=graphene.Date(),
+        # open filters
+        open__gte=graphene.Float(),
+        open__lte=graphene.Float(),
+        # close filters
+        close__gte=graphene.Float(),
+        close__lte=graphene.Float(),
+        # high filters
+        high__gte=graphene.Float(),
+        high__lte=graphene.Float(),
+        # low
+        low__gte=graphene.Float(),
+        low__lte=graphene.Float()
+
+    )
     def resolve_price_bars(self, info, **kwargs):
         return PriceBars.objects.filter(**kwargs)
 
-    partial_price_bar = graphene.List(PriceBarsType)
+    partial_price_bar = graphene.List(
+        PriceBarsType,
+        # date filters
+        datetime_reference__gte=graphene.Date(),
+        datetime_reference__lte=graphene.Date(),
+        # open filters
+        open__gte=graphene.Float(),
+        open__lte=graphene.Float(),
+        # close filters
+        close__gte=graphene.Float(),
+        close__lte=graphene.Float(),
+        # high filters
+        high__gte=graphene.Float(),
+        high__lte=graphene.Float(),
+        # low
+        low__gte=graphene.Float(),
+        low__lte=graphene.Float()
+    )
     def resolve_partial_price_bar(self, info, **kwargs):
         return PartialPriceBar.objects.filter(**kwargs)
 
